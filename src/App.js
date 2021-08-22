512dd25e4fc9a7716122d93fe7b9da997b6d715e
Tom <45665177+TomTom2019@users.noreply.github.com>
2021-08-22 14:31:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/header';
+import Home from './components/home';
+
+const App = () => {
+
+
+
+  
+  return (
+    <BrowserRouter>
+      <Header/>
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
