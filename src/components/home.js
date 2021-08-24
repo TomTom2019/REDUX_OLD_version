4b2a678a6ad1d6cefc8e20748f1d076fc1bb0831
Tom <45665177+TomTom2019@users.noreply.github.com>
2021-08-24 14:21:25 +0200

    message

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux"; // CONNECT
+import { moviesList } from "../store/actions";
 
-const Home = () => {
+class Home extends Component {
+  getMoviesHandler() {
+    this.props.dispatch(moviesList()); // DISPATCH NEED ACTION.JS
+    // DISPATCH SENT TO MOVIES_REDUCER.JS
+  }
 
-    return(
-        <>
-            Home
-        </>
-    )
+  render() {
+    console.log(this.props); // CONNECT INJET PROPS AND HAVE DISPATCH
+    return (
+      <>
+        <button onClick={() => this.getMoviesHandler()}>Get movies</button>
+      </>
+    );
+  }
 }
 
-export default Home;
+export default connect()(Home); //CONNECT WILL INJET PROPS IN HOME
