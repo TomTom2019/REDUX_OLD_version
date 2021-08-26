f53682ca1a532339beb5d055ee7d519b6e93fee2
Tom <45665177+TomTom2019@users.noreply.github.com>
2021-08-26 14:03:51 +0200

    message

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
-import { connect } from "react-redux"; // CONNECT
+import { connect } from "react-redux";
 import { moviesList } from "../store/actions";
 
 class Home extends Component {
   getMoviesHandler() {
-    this.props.dispatch(moviesList()); // DISPATCH NEED ACTION.JS
-    // DISPATCH SENT TO MOVIES_REDUCER.JS
+    this.props.dispatch(moviesList());
   }
 
   render() {
-    console.log(this.props); // CONNECT INJET PROPS AND HAVE DISPATCH
+    console.log(this.props); // GIVE PROPS MOVIES
+    const { movies } = this.props;
     return (
       <>
+        {movies && movies.moviesList
+          ? movies.moviesList.map((
+              item // GET DATA HERE
+            ) => <div key={item.name}>name: {item.name}</div>)
+          : null}
         <button onClick={() => this.getMoviesHandler()}>Get movies</button>
       </>
     );
   }
 }
 
-export default connect()(Home); //CONNECT WILL INJET PROPS IN HOME
+const mapStateToProps = (state) => {
+  // GET DATA WITH MAPSTATETOPROPS
+  return { movies: state.movies };
+};
+
+export default connect(mapStateToProps)(Home); // IN CHARGE GOING TO STORE STATE
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,12 @@
+
+
 export const moviesList = () => {
-  return {
-    type: "MOVIES_LIST", // MOVIES_LIST FROM MOVIES_REDUCER
-    payload: [
-      { id: 1, name: "Pulp fiction" },
-      { id: 2, name: "Pacific Rim" },
-      { id: 3, name: "Rambo" },
-    ],
-  };
-};
+    return {
+        type:'MOVIES_LIST',
+        payload:[
+            {id:1,name:'Pulp fiction'},
+            {id:2,name:'Pacific Rim'},
+            {id:3,name:'Rambo'}
+        ]
+    }
+}
